a10fbe176c102e4c4af8cd43d0495a3f9cfae86e
Gugu Mokwena <gumokwe022@student.wethinkcode.co.za>
2023-07-09 11:37:08 +0200

    Update faq.tsx

--- src/components/atoms/faq/faq.tsx
+++ src/components/atoms/faq/faq.tsx
@@ -11,16 +11,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>What is your refund policy?</span>
+              <span>What is menstruation and why does it occur?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
             <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
-              If you're unhappy with your purchase for any reason, email us
-              within 90 days and we'll refund you in full, no questions asked.
+              Menstruation is the monthly shedding of the uterine lining, typically lasting 3-7 days. It occurs as a result of hormonal changes in the menstrual cycle.
             </Disclosure.Panel>
           </>
         )}
@@ -29,15 +27,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>Do you offer technical support?</span>
+              <span>How does fertility work and when is a person most fertile?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              Fertility refers to the ability to conceive and have a successful pregnancy. A person is most fertile around the time of ovulation, which usually occurs mid-cycle.
             </Disclosure.Panel>
           </>
         )}
@@ -46,15 +43,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>Do you offer technical support?</span>
+              <span>What are hormones and how do they influence the body?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              Hormones are chemical messengers produced by the body's endocrine system that regulate various bodily functions and processes.
             </Disclosure.Panel>
           </>
         )}
@@ -63,15 +59,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>Do you offer technical support?</span>
+              <span>How does conception occur and what factors influence successful reproduction?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              Conception occurs when sperm fertilizes an egg, typically in the fallopian tubes. Factors influencing successful reproduction include ovulation, healthy sperm and egg, and favorable conditions in the reproductive tract.
             </Disclosure.Panel>
           </>
         )}
@@ -80,15 +75,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>Do you offer technical support?</span>
+              <span>What is contraception?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              Contraception, also known as birth control, refers to methods and techniques used to prevent pregnancy. It allows individuals to make informed decisions about if and when they want to have children.
             </Disclosure.Panel>
           </>
         )}
@@ -97,15 +91,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>Do you offer technical support?</span>
+              <span>When should I start using contraception?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              It is recommended to start using contraception before becoming sexually active to prevent unintended pregnancies and protect against sexually transmitted infections (STIs).
             </Disclosure.Panel>
           </>
         )}
@@ -114,15 +107,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>What do I do post rape?</span>
+              <span>How long does a typical menstrual cycle last?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              The average menstrual cycle lasts around 28 days, but it can vary between individuals.
             </Disclosure.Panel>
           </>
         )}
@@ -131,15 +123,15 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>When is the safest time to have an abortion</span>
+              <span>How can I know if I am pregnant?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              If you have had unprotected sex or suspect you might be pregnant, it's important to take a pregnancy test to confirm. Home pregnancy tests are available over-the-counter at pharmacies and are easy to use. You can also consult a healthcare professional for a pregnancy test and guidance.
+
             </Disclosure.Panel>
           </>
         )}
@@ -148,15 +140,14 @@ export const FAQ = () => {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-              <span>How long does the menstruation cycle take?</span>
+              <span>Is abortion safe?</span>
               <BsChevronUp
-                className={`${
-                  open ? "rotate-180 transform" : ""
-                } h-5 w-5 text-purple-500`}
+                className={`${open ? "rotate-180 transform" : ""
+                  } h-5 w-5 text-purple-500`}
               />
             </Disclosure.Button>
-            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-              No.
+            <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white">
+              Abortion, when performed by trained healthcare professionals in a safe and legal setting, is generally considered a safe medical procedure. Complications are rare, but like any medical procedure, there are some potential risks and side effects. It's crucial to seek abortion services from qualified healthcare providers who can provide appropriate care and support.
             </Disclosure.Panel>
           </>
         )}
