dacc38877abc35a5dd95bb1722f4363c0f5643da
refilwe-m <refilwe.dev@gmail.com>
2023-07-09 11:41:29 +0200

    API integration setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { MainRoute } from "./routing";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
-      {/* <Toaster position="top-center" /> */}
+      <Toaster position="top-center" />
       <BrowserRouter>
         <MainRoute />
       </BrowserRouter>
